8dacc29b80a21c3074e1abe122ad776c0143180e
John Aitchison <john@languageconvo.com>
2020-03-22 09:17:46 -0700

    some automated formatting

--- dis/scripts.js
+++ dis/scripts.js
@@ -1,5 +1,7 @@
-<script type="text/javascript">
-  window.hd_util_params = {
+<
+script
+type = "text/javascript" >
+window.hd_util_params = {
     disable_legacy_search_timers: true,
     enable_linking_headings: true,
     enable_zoomable_images: true,
@@ -14,61 +16,61 @@
         </a>
       </div>
     `
-  };
+};
 
-  function ready(fn) {
+function ready(fn) {
     if (document.readyState != 'loading') {
-      fn();
+        fn();
     } else if (document.addEventListener) {
-      document.addEventListener('DOMContentLoaded', fn);
+        document.addEventListener('DOMContentLoaded', fn);
     } else {
-      document.attachEvent('onreadystatechange', function() {
-        if (document.readyState != 'loading') {
-          fn();
-        }
-      });
+        document.attachEvent('onreadystatechange', function () {
+            if (document.readyState != 'loading') {
+                fn();
+            }
+        });
     }
-  }
+}
 
-  function addClass(el, className) {
+function addClass(el, className) {
     if (!el || !className) return;
     if (el.classList) {
-      el.classList.add(className);
+        el.classList.add(className);
     } else {
-      el.className += ' ' + className;
+        el.className += ' ' + className;
     }
-  }
+}
 
-  function removeClass(el, className) {
+function removeClass(el, className) {
     if (el.classList) {
-      el.classList.remove(className);
+        el.classList.remove(className);
     } else {
-      el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+        el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
     }
-  }
+}
 
-  // Toggles menu bar SVG from 3 bars to a cross
-  (function() {
-    ready(function() {
-      var mobileNav = document.querySelector('.hnMobileNavButton');
+// Toggles menu bar SVG from 3 bars to a cross
+(function () {
+    ready(function () {
+        var mobileNav = document.querySelector('.hnMobileNavButton');
 
-      mobileNav.addEventListener('click', function() {
-        if (mobileNav && mobileNav.classList.contains('active')) {
-          removeClass(mobileNav, 'active');
-        } else {
-          addClass(mobileNav, 'active');
-        }
-      });
+        mobileNav.addEventListener('click', function () {
+            if (mobileNav && mobileNav.classList.contains('active')) {
+                removeClass(mobileNav, 'active');
+            } else {
+                addClass(mobileNav, 'active');
+            }
+        });
     });
-  })();
+})();
 
-  // ** Add class when < 3 categories to avoid stretched cards **
-  (function() {
-    ready(function() {
-      var categories = document.getElementById('categories');
-      if (!categories) return;
-      var cards = categories.querySelectorAll('.card');
-      if (cards && cards.length <= 3) addClass(categories, 'fewCategories');
+// ** Add class when < 3 categories to avoid stretched cards **
+(function () {
+    ready(function () {
+        var categories = document.getElementById('categories');
+        if (!categories) return;
+        var cards = categories.querySelectorAll('.card');
+        if (cards && cards.length <= 3) addClass(categories, 'fewCategories');
     });
-  })();
+})();
 </script>
